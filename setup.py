eb80540054247091952f689e64d6ab48836d6fa9
Andrew B. Martin <andrew.brown.martin@gmail.com>
2016-08-04 12:15:32 -0400

    All methods available in top level module

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pygurobi',
   packages = ['pygurobi'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'Rapid interactive Gurobi model modification and analysis',
   author = 'Andrew B. Martin',
   author_email = 'andrew.brown.martin@gmail.com',
   url = 'https://github.com/AndrewBMartin/pygurobi', # use the URL to the github repo
-  download_url = 'https://github.com/AndrewBMartin/pygurobi/tarball/0.1', # I'll explain this in a second
-  keywords = ['gurobi', 'optimization', 'interactive'], # arbitrary keywords
+  download_url = 'https://github.com/AndrewBMartin/pygurobi/tarball/0.3',
+  keywords = ['operations research', 'optimization', 'interactive'], # arbitrary keywords
   classifiers = [],
 )
