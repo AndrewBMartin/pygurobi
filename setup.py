0a1fac4abaab9a411b46c8b607b1b56ba288ced2
andrew <andrew@ecomodelling.co>
2017-03-23 16:31:16 -0400

    Configuring for PyPi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pygurobi',
   packages = ['pygurobi'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'Rapid interactive Gurobi model modification and analysis',
   author = 'Andrew B. Martin',
   author_email = 'andrew.brown.martin@gmail.com',
   url = 'https://github.com/AndrewBMartin/pygurobi', # use the URL to the github repo
-  download_url = 'https://github.com/AndrewBMartin/pygurobi/tarball/0.3',
+  download_url = 'https://github.com/AndrewBMartin/pygurobi/tarball/0.4',
   keywords = ['operations research', 'optimization', 'interactive'], # arbitrary keywords
   classifiers = [],
 )
